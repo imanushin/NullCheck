04b903c1de0acfbf220364d16b5066fae73f8339
Igor Manushin <igor.manushin@gmail.com>
2015-05-31 00:16:37 +0300

    add solution

---
+++ NullCheck/NullCheckAnalyzer/NullCheckAnalyzer.Test/UnitTests.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestHelper;
+using NullCheckAnalyzer;
+
+namespace NullCheckAnalyzer.Test
+{
+    [TestClass]
+    public class UnitTest : CodeFixVerifier
+    {
+
+        //No diagnostics expected to show up
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var test = @"";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        internal sealed class TypeName
+        {
+            public TypeName(string value)
+            {
+                Value = value;
+            }
+            
+            public string Value
+            {
+                get;
+                private set;
+            }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = NullCheckAnalyzerAnalyzer.DiagnosticId,
+                Message = string.Format(NullCheckAnalyzerAnalyzer.DiagnosticMessageFormat, ".ctor", "TypeName", "value"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 11, 15)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+
+            const string fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        internal sealed class TypeName
+        {
+            public TypeName([NotNull]string value)
+            {
+                Value = value;
+            }
+            
+            public string Value
+            {
+                get;
+                private set;
+            }
+        }
+    }";
+            VerifyCSharpFix(test, fixtest);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new NullCheckAnalyzerCodeFixProvider();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new NullCheckAnalyzerAnalyzer();
+        }
+    }
+}
---
+++ NullCheck/NullCheckAnalyzer/NullCheckAnalyzer/NullCheckAnalyzerAnalyzer.cs
@@ -0,0 +1,46 @@
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace NullCheckAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class NullCheckAnalyzerAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "NullCheckAnalyzer_MethodContainNulls";
+        public const string DiagnosticMessageFormat = "Method {0} in type {1} does not have null/not null attribute for argument {2}";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        internal static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        internal static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        internal static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        internal const string Category = "Naming";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
+            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+
+            // Find just those named type symbols with names containing lowercase letters.
+            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+            {
+                // For all such symbols, produce a diagnostic.
+                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
