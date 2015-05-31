a877de867e8a8b4077b45529c860462000b28bd6
Igor Manushin <igor.manushin@gmail.com>
2015-06-01 00:45:48 +0300

    spelling fix

--- NullCheck/NullCheckAnalyzer/NullCheckAnalyzer/NullCheckAnalyzer.cs
+++ NullCheck/NullCheckAnalyzer/NullCheckAnalyzer/NullCheckAnalyzer.cs
@@ -8,7 +8,7 @@ namespace NullCheckAnalyzer
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class NullCheckAnalyzer : DiagnosticAnalyzer
     {
-        public const string ParameterIsNullId = "NullCheckAnalyzer_MethodContainNulls";
+        public const string ParameterIsNullId = "NullCheckAnalyzer_MethodContainsNulls";
 
         // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
         internal static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
